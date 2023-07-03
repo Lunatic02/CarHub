988f28df6531d719e26b2ce57757f9b7f0d88fae
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-07-03 19:50:16 -0300

    search bar

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,11 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': 'f1bbb17469msh3b47878d3192f79p11a14fjsne20a2bae658e',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
-  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { headers: headers }
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=gol', { headers: headers }
   );
 
   const result = await response.json()
@@ -24,5 +26,6 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
 
 
