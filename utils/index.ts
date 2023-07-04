c6e52df7d40aaffadbaac78de126b5868d902382
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-07-04 20:11:32 -0300

    page

--- utils/index.ts
+++ utils/index.ts
@@ -36,7 +36,7 @@ export const updateSearchParams = (type: string, value: string)=>{
 
     const newPathname=`${window.location.pathname}?${searchParams.toString()}`
 
-    return newPathname;
+    return newPathname
 }
 
 
